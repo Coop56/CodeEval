efa651349bdfca6fe3f2297cab631a9fb0313ba5
Jeremiah Cooper <JCooper@katemiah.com>
2015-12-08 11:30:32 -0500

    Javascript: Odd Numbers

---
+++ 1. Easy Level/Odd Numbers/main.js
@@ -0,0 +1,11 @@
+/* 
+
+Code Eval Challenge - Javascript - Easy - Odd Numbers - https://www.codeeval.com/open_challenges/25/
+
+CHALLENGE DESCRIPTION: Print the odd numbers from 1 to 99.
+
+*/
+
+for (i = 1; i <= 99; i = i + 2) {
+    console.log(i);
+}
