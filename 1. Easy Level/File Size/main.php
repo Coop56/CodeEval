201cf062adde5afb813bf6a7f5420d6f3afa2563
Jeremiah Cooper <JCooper@katemiah.com>
2015-12-08 11:30:05 -0500

    PHP: File Size

---
+++ 1. Easy Level/File Size/main.php
@@ -0,0 +1,11 @@
+/* 
+
+Code Eval Challenge - PHP - Easy - File Size - https://www.codeeval.com/open_challenges/26/
+
+CHALLENGE DESCRIPTION: Print the size of a file in bytes.
+
+*/
+
+<?php
+echo filesize($argv[1]);
+?>
